de42579d0772970e54f32cb28655ded2fcea1eba
laminet2 <kamiya.traore@gmail.com>
2023-12-16 08:03:04 +0000

    Prevision impecte mais Style a corriger

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-## ML-Model-Flask-Deployment
-This is a demo project to elaborate how Machine Learn Models are deployed on production using Flask API
-
-### Prerequisites
-You must have Scikit Learn, Pandas (for Machine Leraning Model) and Flask (for API) installed.
 
 ### Project Structure
 Ce projet a etet construit sous le framework flask :
@@ -12,6 +7,7 @@ Il faudra lancer le terminal et taper les commandes sur le terminal :
 ```python
 pip install scikit-learn==1.2.2
 pip install numpy
+pip install pandas
 pip install flask
 python app.py
 ```
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -14,13 +15,18 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    # dump(request.form.values())
+    # int_features = [int(x) for x in request.form.values()]
+    # final_features = [np.array(int_features)]
+
+    data={"Medications":request.form['Medications'],"Previous Diagnoses":request.form['PreviousDiagnoses'],"Anxiety Diagnosis":request.form['AnxietyDiagnosis'],"Obsession Type":request.form['ObsessionType'],"Education Level":request.form['EducLevel']}
+    df = pd.DataFrame(data,index=[0])
+    prediction = model.predict(df)
+
 
     output = round(prediction[0], 2)
     outputSentence="une depression" if output==1 else "aucune depression"
-    return render_template('index.html', prediction_text=f"Le patient a  $ {outputSentence}")
+    return render_template('index.html', prediction_text=f"Le patient a   {outputSentence}")
 
 
 
